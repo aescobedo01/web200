2a0e03aa332127e29082db9ac648e8797cc7f81b
anaescobedo <ana.escbedo@gmail.com>
2023-09-01 21:45:03 -0500

    updateee

--- web200/assignment1/script.js
+++ web200/assignment1/script.js
@@ -1,7 +1,10 @@
+/* javascript rocks should shhow up in blue*/
 let firstid = document.getElementById('firstid')
 firstid.innerHTML = "JavaScript rocks!";
 document.getElementById("firstid").style.color = "blue";
 
+/*javascript is awesome should show up iin popup window*/
 window.alert("JavaScript is awesome.");
 
+/*shows up in console*/
 console.log("lol hopefully this message shows up.");
