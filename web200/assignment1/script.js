bc85d2f77e8b15e0d72e353a69fbc702d28c89b5
anaescobedo <ana.escbedo@gmail.com>
2023-09-01 21:24:40 -0500

    update

--- web200/assignment1/script.js
+++ web200/assignment1/script.js
@@ -1,2 +1,7 @@
 let firstid = document.getElementById('firstid')
 firstid.innerHTML = "JavaScript rocks!";
+document.getElementById("firstid").style.color = "blue";
+
+window.alert("JavaScript is awesome.");
+
+console.log("lol hopefully this message shows up.");
