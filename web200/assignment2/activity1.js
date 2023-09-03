975d6bcdfee3fffae850aabac96a66a04d810434
anaescobedo <ana.escbedo@gmail.com>
2023-09-03 16:41:09 -0500

    update asn2

--- web200/assignment2/activity1.js
+++ web200/assignment2/activity1.js
@@ -1,15 +1,9 @@
 function returnText() {
-    let wage = parseFloat(document.getElementById("hoursInput").value);
-    let hours = parseFloat(document.getElementById("wageInput").value);
+    let hours = parseFloat(document.getElementById("hoursInput").value);
+    let wage = parseFloat(document.getElementById("wageInput").value);
     const calculateYearly = hours * wage * 52;
     const calculateMonthly = hours * wage * 4;
     const calculateWeekly = hours * wage * 1;
 
     window.alert(["Your weekly salary is " + calculateWeekly + ", your monthly salary is " + calculateMonthly + ", and your yearly salary is " + calculateYearly +"."]);
-
-    //const wage = returntext();
-    
-    //document.getElementById("totalSalary").innerHTML = calculateYearly;
-    //document.getElementById("totalSalary").innerHTML = calculateMonthly;
-    //document.getElementById("totalSalary").innerHTML = calculateWeekly;
 }
--- web200/assignment2/activity2.js
+++ web200/assignment2/activity2.js
@@ -1,1 +1,25 @@
+function returnText() {
+    var age = parseFloat(document.getElementById("ageInput").value);
 
+    const monthsPerYear = 12;
+    const daysPerYear = 365;
+    const hoursPerDay = 24;
+    const secondsPerHour = 3600;
+
+    const calculateMonths = age * monthsPerYear;
+    const calculateDays = age * daysPerYear;
+    const calculateHours = age * daysPerYear * hoursPerDay;
+    const calculateSeconds = age * hoursPerDay * secondsPerHour;
+
+    window.alert(["Your age in months is " + calculateMonths + " months, your age in days is " + calculateDays + " days, your age in hours is " + calculateHours + " hours, and your age in seconds is " + calculateSeconds + " seconds."]);
+}
+
+/*unsure how to add the result in the console log
+
+var age = returnText();
+
+console.log("Your age in months is:", age.calculateMonths);
+console.log("Your age in days is:", age.calculateDays);
+console.log("Your age in hours is:", age.calculateHours);
+console.log("Your age in seconds is:", age.calculateSeconds);
+*/
