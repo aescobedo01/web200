9e9cae3ef1038aec66f400c16122c84bc2fdbdc5
mheliker <mheliker@harpercollege.edu>
2023-09-06 17:35:01 -0500

    MPH demo of function return statement.

--- web200/assignment3/activity4.js
+++ web200/assignment3/activity4.js
@@ -5,15 +5,26 @@ shape choices. That will come later. For now, just
 include multiple shape calculations in sequence
 */
 
-function returnText() {
-    //
+// this function returns the area of a rectangle only. 
+function rectangleArea() {
+
     let widthofr = parseFloat(document.getElementById("widthRec").value);
     let heightofr = parseFloat(document.getElementById("heightRec").value);
-    let widthoft = parseFloat(document.getElementById("widthTri").value);
-    let heightoft = parseFloat(document.getElementById("heightTri").value);
+    // let widthoft = parseFloat(document.getElementById("widthTri").value);
+    // let heightoft = parseFloat(document.getElementById("heightTri").value);
 
-    const calcTri = widthoft * heightoft * .5;
+    // const calcTri = widthoft * heightoft * .5;
+    //return widthofr * heightofr;
+    
+    // calculate area of rectangle
     const calcRec = widthofr * heightofr;
 
-    document.getElementById("results").innerHTML = (['The area of a triangle is ' + calcTri + ', and the are of a rectangle is ' + calcRec + '.']);
+    // return area
+    return calcRec; 
+
+}
+
+// this function calls the rectangleArea function to get the value of the area of a rectangle. 
+function returnRectangle(){
+    document.getElementById("results").innerHTML = 'The are of a rectangle is ' + rectangleArea() + '.';
 }
