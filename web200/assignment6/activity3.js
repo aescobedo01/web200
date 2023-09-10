a5e1e69596e5cfcc0a71c7db29a082c28ce94b61
anaescobedo <ana.escbedo@gmail.com>
2023-09-10 00:51:50 -0500

    update

---
+++ web200/assignment6/activity3.js
@@ -0,0 +1,7 @@
+/*  ask the user how many scores they would like to enter. 
+    Then use a loop to request each score using a prompt and 
+    add it to a total. Finally, calculate and display the average 
+    for the entered scores.
+    */
+
+    
